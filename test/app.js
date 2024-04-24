dc65c97e53fb747a1b202a5bca30518c729b16d6
Gaurav K. Prasad <gaurav14112005@gmail.com>
2024-04-24 11:55:18 +0530

    testing complete implementing grid in body

---
+++ test/app.js
@@ -0,0 +1,11 @@
+let nav = document.querySelector(".nav");
+let text = document.querySelector(".text");
+let cover = document.querySelector('.cover');
+
+cover.addEventListener("click", () => {
+    console.log("cover");
+})
+
+text.addEventListener("click", () => {
+    console.log("text");
+})
